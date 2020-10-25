7effd25b51eb4c7da4dfc6cbc06c02d7457ba912
aryaa404 <71863153+aryaa404@users.noreply.github.com>
2020-10-25 13:20:05 +0700

    Delete checknet.bat

--- FILE/checknet.bat
+++
@@ -1,7 +0,0 @@
-@echo off
-echo checking internet connection
-Ping www.google.nl -n 1 -w 1000
-cls
-if errorlevel 1 (set internet=Not connected to internet) else (set internet=Connected to internet)
-
-echo %internet%
