0519e009013aae55fe017caf4c632ffe28d9fc7d
aryaa404 <71863153+aryaa404@users.noreply.github.com>
2020-10-25 13:18:00 +0700

    Delete check_internet_tray.sh

--- FILE/check_internet_tray.sh
+++
@@ -1,4 +0,0 @@
-online() {
-    find /sys/class/net/ -maxdepth 1 -mindepth 1 ! -name "*lo*" -exec sh -c 'cat "$0"/carrier 2>&1 > /dev/null | grep -q "1" && exit 0' {} \;
-}
-online && gnubiff --systemtray --noconfigure &
