208ec5517ecbe6cb1c54c0fb2aa611e105abc87b
G33k56 <59796387+G33k56@users.noreply.github.com>
2020-10-28 10:06:08 +0700

    Create checknet_gateway.sh

---
+++ FILE/checknet_gateway.sh
@@ -0,0 +1,7 @@
+GATEWAY=`route -n get default | grep gateway`
+if [ -z "$GATEWAY" ]
+  then
+    echo error
+else
+  ping -q -t 1 -c 1 `echo $GATEWAY | cut -d ':' -f 2` > /dev/null && echo ok || echo error
+fi
